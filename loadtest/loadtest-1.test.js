599058995c9371c21261bc0cce67a1f98d5d1029
shysssthanhtri <shyssthanhtri@gmail.com>
2025-02-18 08:02:57 +0700

    feat: add more test

--- loadtest/loadtest-1.test.js
+++ loadtest/loadtest-1.test.js
@@ -1,4 +1,5 @@
 import http from 'k6/http';
+import { check } from 'k6';
 
 /**
  * @type {import("k6/options").Options}
@@ -19,10 +20,11 @@ export const options = {
     },
   ],
   thresholds: {
-    http_req_duration: ['p(90)<100'],
+    http_req_duration: ['p(90)<10'],
   },
 };
 
 export default function () {
   http.get('http://localhost:3000');
+  check(res, { 'status is 200': (res) => res.status === 200 });
 }
